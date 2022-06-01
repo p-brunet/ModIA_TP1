2984592b0c18a5811ed1fd54ba8fee0851f03dac
DavidBert <davidbertoin@gmail.com>
2022-06-01 17:52:46 +0200

    TP1

--- train_mnist.py
+++ train_mnist.py
@@ -14,7 +14,7 @@ from mnist_net import MNISTNet
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(net, optimizer, loader, epochs=10):
+def train(net, optimizer, loader, writer, epochs=10):
     criterion = nn.CrossEntropyLoss()
     for epoch in range(epochs):
         running_loss = []
@@ -28,6 +28,7 @@ def train(net, optimizer, loader, epochs=10):
             loss.backward()
             optimizer.step()
             t.set_description(f'training loss: {mean(running_loss)}')
+            writer.add_scalar('training loss', mean(running_loss), epoch)
 
 def test(model, dataloader):
     test_corrects = 0
@@ -43,38 +44,55 @@ def test(model, dataloader):
 
 if __name__=='__main__':
 
-  parser = argparse.ArgumentParser()
-  
-  parser.add_argument('--exp_name', type=str, default = 'MNIST', help='experiment name')
-  parser.add_argument(...)
-  parser.add_argument(...)
-  parser.add_argument(...)
+    writer = SummaryWriter(f'runs/MNIST')
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--exp_name', type=str, default = 'MNIST', help='experiment name')
+    parser.add_argument('--batch_size', type = int, default= 64, help = 'batch size')
+    parser.add_argument('--learning_rate', type= float, default= 1e-3, help ='learning rate')
+    parser.add_argument('--epochs', type = int, default= 10, help = 'number of epochs')
 
-  args = parser.parse_args()
-  exp_name = args.exp_name
-  epochs = ...
-  batch_size = ...
-  lr = ...
+    args = parser.parse_args()
+    exp_name = args.exp_name
+    epochs = args.epochs
+    batch_size = args.batch_size
+    lr = args.learning_rate
 
-  # transforms
-  transform = transforms.Compose(
-      [transforms.ToTensor(),
-      transforms.Normalize((0.5,), (0.5,))])
+    # transforms
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))])
 
-  # datasets
-  trainset = torchvision.datasets.MNIST('./data', download=True, train=True, transform=transform)
-  testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
+    # datasets
+    trainset = torchvision.datasets.MNIST('./data', download=True, train=True, transform=transform)
+    testset = torchvision.datasets.MNIST('./data', download=True, train=False, transform=transform)
 
-  # dataloaders
-  trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-  testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-  
-  net = ...
-  # setting net on device(GPU if available, else CPU)
-  net = net.to(device)
-  optimizer = optim.SGD(...)
+    # dataloaders
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    net = MNISTNet()
+    # setting net on device(GPU if available, else CPU)
+    net = net.to(device)
+    optimizer = optim.SGD(net.parameters(),lr=lr,momentum= 0.9)
 
-  train(...)
-  test_acc = test(...)
-  print(f'Test accuracy:{test_acc}')
-  torch.save(net.state_dict(), "mnist_net.pth")
+    train(net, optimizer,trainloader,writer, epochs)
+    test_acc = test(net,testloader)
+    print(f'Test accuracy:{test_acc}')
+    torch.save(net.state_dict(), "mnist_net.pth")
+    
+    #add embeddings to tensorboard
+    perm = torch.randperm(len(trainset.data))
+    images, labels = trainset.data[perm][:256], trainset.targets[perm][:256]
+    images = images.unsqueeze(1).float().to(device)
+    with torch.no_grad():
+        embeddings = net.get_features(images)
+        writer.add_embedding(embeddings,
+                    metadata=labels,
+                    label_img=images, global_step=1)
+
+    # save networks computational graph in tensorboard
+    writer.add_graph(net, images)
+    # save a dataset sample in tensorboard
+    img_grid = torchvision.utils.make_grid(images[:64])
+    writer.add_image('mnist_images', img_grid)
